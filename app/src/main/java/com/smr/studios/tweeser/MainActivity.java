0c142391559a5c11b0479e24289fc184e89abcb0
Unknown <unknown@example.com>
2019-10-20 16:16:30 -0400

    Main Screen Done

--- app/src/main/java/com/smr/studios/tweeser/MainActivity.java
+++ app/src/main/java/com/smr/studios/tweeser/MainActivity.java
@@ -1,13 +1,25 @@
 package com.smr.studios.tweeser;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void startSearchEngine(View view)
+    {
+        Intent intent = new Intent(this,SearchEngine.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/smr/studios/tweeser/SearchEngine.java
@@ -0,0 +1,14 @@
+package com.smr.studios.tweeser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchEngine extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_engine);
+    }
+}
